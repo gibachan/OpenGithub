57fd9d81cb2f903a48d94b0f3fb1aa26f277c201
gibachan <childhoodend.tk@gmail.com>
2018-05-26 18:25:12 +0900

    Add new command

--- OpenGithubExtension/SourceEditorCommand.swift
+++ OpenGithubExtension/SourceEditorCommand.swift
@@ -30,3 +30,28 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     }
     
 }
+
+class OpenPRCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        let connection = NSXPCConnection(serviceName: "jp.cat-soft.OpenGithubHelper")
+        connection.remoteObjectInterface = NSXPCInterface(with: OpenGithubHelperProtocol.self)
+        connection.resume()
+        let helper = connection.remoteObjectProxy as! OpenGithubHelperProtocol
+        
+        var line = 0
+        let textBuffer = invocation.buffer
+        if let selection = textBuffer.selections.firstObject as? XCSourceTextRange {
+            line = selection.start.line + 1
+        }
+        
+        let semaphore = DispatchSemaphore(value: 0)
+        helper.openPR(with: String(line)) {
+            semaphore.signal()
+        }
+        _ = semaphore.wait(timeout: .now() + 5)
+        completionHandler(nil)
+    }
+    
+}
